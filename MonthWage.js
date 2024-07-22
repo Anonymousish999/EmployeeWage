5f57c934ee569cb84d608fe7b3b7fcefae39cfca
Ishu Gupta <guptarhsu999@gmail.com>
2024-07-22 22:25:54 +0530

    [ADD] calculate wage using func

--- MonthWage.js
+++ MonthWage.js
@@ -1,19 +1,9 @@
 
-function monthlyWage(dailyWage, hours) {
-    const workingDays = 20;
-    const totalHours = hours * workingDays;
-    let wage = 0;
+const wage = 20;
+const hour = 8;
 
-    if(totalHours >= 100 && workingDays >= 20) {
-        
-        wage = dailyWage * totalHours;
-    }
-
-    return wage;
+function employeeWage(){
+    return hour * wage;
 }
 
-const dailyWage = 20;
-const hours = 8;
-const monthlyWages = monthlyWage(dailyWage, hours);
-console.log(`Monthly wage: ${monthlyWages}`);
-
+console.log(`The total wage is ${employeeWage()}`);
